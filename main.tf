97716180c616132bc5caba17946fcd154fb29782
Kacper Krzywiec <krzywiec@gmail.com>
2023-03-03 15:00:13 +0100

    test vercel

--- main.tf
+++ main.tf
@@ -15,16 +15,29 @@ terraform {
   }
 }
 
-variable "is_prod" {
-  description = "If false, deploys preview environment"
-  default     = false
+# variable "is_prod" {
+#   description = "If false, deploys preview environment"
+#   default     = false
+# }
+
+# module "preview_env" {
+#   source  = "./preview-env"
+#   is_prod = var.is_prod
+# }
+
+# output "preview_url" {
+#   value = module.preview_env.preview_url
+# }
+data "vercel_project_directory" "build_path" {
+  path = "public"
+}
+resource "vercel_project" "mc_app_test" {
+  name      = "mc-app-test-deployments-test"
 }
 
-module "preview_env" {
-  source  = "./preview-env"
-  is_prod = var.is_prod
-}
-
-output "preview_url" {
-  value = module.preview_env.preview_url
+resource "vercel_deployment" "mc_app_test" {
+  project_id  = vercel_project.mc_app_test.id
+  files       = data.vercel_project_directory.build_path.files
+  path_prefix = "public"
+  production  = true
 }
--- preview-env/variables.tf
+++ preview-env/variables.tf
@@ -1,8 +1,3 @@
-variable "environment_tag" {
-  description = "Environment tag"
-  default     = "Learn"
-}
-
 variable "is_prod" {
   description = "If false, deploys preview environment"
   default     = false
